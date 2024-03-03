7c7b5bb9db7b3cc0131e48cf753a22031326c5d8
KissesJun <49674629+GWillS163@users.noreply.github.com>
2024-03-03 18:14:06 +0900

    add readme

--- src/utils/puzzleCracker.js
+++ src/utils/puzzleCracker.js
@@ -3,7 +3,7 @@
  */
 export default{
     
-    hostUrl: "http://192.168.4.143:3000",
+    hostUrl: "http://192.168.4.143:3000", // no using
     getFullImageUrl(url) {
         return hostUrl + url;
     }
